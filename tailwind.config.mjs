9b6c7184ea6c43ac3e6a19bbfd496e2c3414a926
uzaifkhan-2774 <uzaifkhan248@gmail.com>
2024-12-24 12:57:29 +0530

    font changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
     },
   },
   plugins: [],
